8f5487105a706f92143ccd53c45cde3fdf22b399
Monica Yamazaki <monicasayama@gmail.com>
2019-11-08 19:32:19 -0300

    salva usuario q loga pelo google

--- src/pages/login.js
+++ src/pages/login.js
@@ -21,8 +21,22 @@ function signIn() {
 function google() {
   const provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithPopup(provider)
-    .then(() => {
-      window.location = '#feed';
+    .then((currentUser) => {
+      const usersCollection = firebase.firestore().collection('users');
+      usersCollection.where('user_uid', '==', currentUser.user.uid).get()
+        .then((snap) => {
+          if (snap.size === 0) {
+            const user = {
+              nome: currentUser.additionalUserInfo.profile.given_name,
+              sobrenome: currentUser.additionalUserInfo.profile.family_name,
+              user_uid: currentUser.user.uid,
+            };
+            firebase.firestore().collection('users').add(user);
+            window.location = '#feed';
+          } else {
+            window.location = '#feed';
+          }
+        });
     });
 }
 
